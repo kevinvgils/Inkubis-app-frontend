ec515ffbfc289bb1704c43cf62803154718aee46
Junhao Chen <junhao@hotmail.com>
2022-12-22 09:59:24 +0100

    Add default path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -33,6 +33,7 @@ const routes: Routes = [
   },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: '**', redirectTo: 'login' },
 ];
 
 @NgModule({
