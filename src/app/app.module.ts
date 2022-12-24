9c35cb4e5aedf032afa513c1422314e4f544d8bb
Junhao Chen <junhao@hotmail.com>
2022-12-24 19:15:54 +0100

    Add register validation and converted to formBuilder and improve login validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { AppComponent } from './app.component';
 import { FormComponent } from './components/form/form.component';
 import { LoginComponent } from './components/auth/login/login.component';
 import { RegisterComponent } from './components/auth/register/register.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { PdfComponent } from './pdf/pdf.component';
 import { LoggedInAuthGuard } from './components/auth/auth.guards';
 
@@ -25,6 +25,7 @@ import { LoggedInAuthGuard } from './components/auth/auth.guards';
     RouterModule,
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [LoggedInAuthGuard],
   bootstrap: [AppComponent],
