d475cf1590073d837559835a463b17b826269dd4
Tim Aerts <t.aerts@hotmail.com>
2023-01-03 23:40:02 +0100

    Fixed the template
    Did nested formgroups wrong

--- src/app/contract/contract.component.ts
+++ src/app/contract/contract.component.ts
@@ -17,9 +17,9 @@ export class ContractComponent extends FormProvider implements OnInit {
 
     contractinfo: new FormGroup({
       companyResponsibleForDataProcessing: new FormGroup({
-        name: new FormControl(''),
+        name: new FormControl(""),
         legalCountry: new FormControl(false),
-        companyNumber: new FormControl(''),
+        companyNumber: new FormControl(""),
         address: new FormControl(''),
         zipcode: new FormControl(''),
         city: new FormControl(''),
