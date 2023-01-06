425ed12a3cd9e6907e6db5647ed2e763dcb51cee
Thijs Raaijmakers <TRaaijmakers32@gmail.com>
2023-01-06 21:30:52 +0100

    Added basic html for each contract

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Contract } from '../contract/contract';
 
 @Component({
   selector: 'app-homepage',
@@ -6,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./homepage.component.css']
 })
 export class HomepageComponent implements OnInit {
-
+  contracts: Contract[] = []; //getAllContracts();
+  
   constructor() { }
 
   ngOnInit(): void {
