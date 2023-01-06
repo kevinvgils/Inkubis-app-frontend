1eca02df0b5c132a3b829f49d3883d809e11224f
Thijs Raaijmakers <TRaaijmakers32@gmail.com>
2023-01-06 17:32:36 +0100

    Functionality start

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Contract } from '../contract/contract';
 
 @Component({
   selector: 'app-admin',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent implements OnInit {
+  contracts: Contract[] = []; //getAllContracts();
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  getAllContracts() {
+    return ;
+  }
 }
