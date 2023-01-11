a22803f8daf2711e862fab3fb9f127ebfdc4e399
Tim Aerts <t.aerts@hotmail.com>
2023-01-11 14:03:01 +0100

    Quick changes

--- src/app/contract/contract-routing.module.ts
+++ src/app/contract/contract-routing.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoggedInAuthGuard } from '../auth/auth.guards';
 import { ContractComponent } from './contract.component';
-import { FirstPartComponent } from './first-part/first-part.component';
-import { SecondPartComponent } from './second-part/second-part.component';
 import { ContractInfoComponent } from './categories/contract-info/contract-info.component';
 import { ContractSigneesComponent } from './categories/contract-signees/contract-signees.component';
 import { ThirdPartyDataComponent } from './categories/third-party-data/third-party-data.component';
